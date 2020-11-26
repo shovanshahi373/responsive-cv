daedf2569e0da8eda0c295700826efb23e3a3d6c
sovan <sovanshahihero@gmail.com>
2020-11-26 18:38:02 +0545

    init

---
+++ assets/js/variables.js
@@ -0,0 +1,82 @@
+export const projects = [
+  {
+    id: 1,
+    title: "a todo-list app",
+    body: "an SPA showcasing a simple TODO-list app",
+  },
+  {
+    id: 2,
+    title: "blogging site",
+    body:
+      "built under the framework of Next JS, this app is a clone of a blogging website that can create blogs, write comments, like blogs etc.",
+  },
+  {
+    id: 3,
+    title: "chat application",
+    body:
+      "a real time end-to-end chatting application built using React, Node and Websockets",
+  },
+  {
+    id: 4,
+    title: "complain registration web app",
+    body:
+      "a user-friendly web application built with the aim of registering garbage problems issued by people so that it can reach to responsible authorities.",
+  },
+  {
+    id: 5,
+    title: "browser based mini-games",
+    body: "games like word-typing, connect-4, hangman",
+  },
+];
+
+export const skills = [
+  {
+    id: 1,
+    name: "HTML",
+    level: "advanced",
+    score: 1,
+    img: "./assets/images/html5.svg",
+  },
+  {
+    id: 2,
+    name: "CSS",
+    level: "intermediate",
+    score: 0.9,
+    img: "./assets/images/css3.svg",
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    level: "intermediate",
+    score: 0.8,
+    img: "./assets/images/js.svg",
+  },
+  {
+    id: 4,
+    name: "NodeJS",
+    level: "intermediate",
+    score: 0.7,
+    img: "./assets/images/node.svg",
+  },
+  {
+    id: 5,
+    name: "ReactJS",
+    level: "intermediate",
+    score: 0.75,
+    img: "./assets/images/react.svg",
+  },
+  {
+    id: 6,
+    name: "Bootstrap",
+    level: "intermediate",
+    score: 0.9,
+    img: "./assets/images/bootstrap.svg",
+  },
+  {
+    id: 7,
+    name: "UI/UX",
+    level: "basic",
+    score: 0.55,
+    img: "./assets/images/uiux.svg",
+  },
+];
---
+++ main.js
@@ -0,0 +1,43 @@
+import { projects, skills } from "./assets/js/variables.js";
+
+function setup() {
+  console.log(projects);
+  $.each(skills, function (i, obj) {
+    const $tr = $("<tr></tr>");
+    const $td = $("<td></td>");
+    const $icon = $("<img></img>").attr("src", obj.img).addClass("icon");
+    $td.append($icon);
+    const $td1 = $("<td></td>").text(obj.name);
+    const $td2 = $("<td></td>");
+    const $skillMeter = $("<span></span>").addClass("skill-meter");
+    const $skillMeterValue = $("<span></span>")
+      .addClass("skill-meter-value")
+      .animate({ width: obj.score * 100 + "%" }, 2000);
+    $skillMeter.append($skillMeterValue);
+    $td2.append($skillMeter);
+    $tr.append($td, $td1, $td2);
+    $(".skill-table").append($tr);
+  });
+  $(".toggle-group").each(function () {
+    $(this).on("click", function () {
+      $(this).parent().next().slideToggle();
+    });
+  });
+
+  $.each(projects, function (i, obj) {
+    const project = $("<div></div>").addClass("project");
+    const projectTitle = $("<h3></h3>")
+      .text(obj.title)
+      .addClass("project-title");
+    projectTitle.on("click", function () {
+      $(this).next().toggleClass("active");
+    });
+    const projectDesc = $("<p></p>").text(obj.body).addClass("project-desc");
+    project.append(projectTitle, projectDesc);
+    $(".projects-list").append(project);
+  });
+}
+
+$(function () {
+  setup();
+});
